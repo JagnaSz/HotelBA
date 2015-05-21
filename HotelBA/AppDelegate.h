1bd383be9a6c07d8ded69d0e91bfe17f68394268
Agnieszka Szczurek <agnieszkaszczurek93@gmail.com>
2015-05-21 18:41:34 +0200

    Initial commit

---
+++ HotelBA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelBA
+//
+//  Created by Agnieszka Szczurek on 21.05.2015.
+//  Copyright (c) 2015 AgnieszkaSzczurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotelBA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HotelBA
+//
+//  Created by Agnieszka Szczurek on 21.05.2015.
+//  Copyright (c) 2015 AgnieszkaSzczurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
